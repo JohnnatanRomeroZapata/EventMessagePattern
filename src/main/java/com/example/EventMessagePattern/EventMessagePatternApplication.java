677298166996c950da60b31dd9a2967e0a378e51
Johnnatan <romerozapatajonathan@gmail.com>
2023-10-18 12:05:15 -0400

    project init

---
+++ src/main/java/com/example/EventMessagePattern/EventMessagePatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventMessagePattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMessagePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMessagePatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EventMessagePattern/EventMessagePatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventMessagePattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMessagePatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
